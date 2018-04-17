37311e72bd19861da850ef0b064cbc99d7b90351
Brian Caicco <2147327+briancaicco@users.noreply.github.com>
2018-04-16 20:28:36 -0500

    new user custom email

--- wp-content/themes/alphamatch-child/functions.php
+++ wp-content/themes/alphamatch-child/functions.php
@@ -604,27 +604,33 @@ return $message;
 
 
 
+// Redefine user notification function
+if ( !function_exists('wp_new_user_notification') ) {
+    function wp_new_user_notification( $user_id, $plaintext_pass = '' ) {
+        $user = new WP_User($user_id);
 
-/**
- * Login Page Customizations
- *
- * @since 0.1.0
- * @uses 
- */
+        $user_login = stripslashes($user->user_login);
+        $user_email = stripslashes($user->user_email);
 
-function alphas_login_logo() { ?>
+        $message  = sprintf(__('New user registration on your blog %s:'), get_option('blogname')) . "\r\n\r\n";
+        $message .= sprintf(__('Username: %s'), $user_login) . "\r\n\r\n";
+        $message .= sprintf(__('E-mail: %s'), $user_email) . "\r\n";
 
-<?php }
-add_action( 'login_enqueue_scripts', 'alphas_login_logo' );
+        @wp_mail(get_option('admin_email'), sprintf(__('[%s] New User Registration'), get_option('blogname')), $message);
 
+        if ( empty($plaintext_pass) )
+            return;
 
-function my_login_stylesheet() {
-    wp_enqueue_style( 'custom-login', get_stylesheet_directory_uri() . '/assets/css/style-login.css' );
-    //wp_enqueue_script( 'custom-login', get_stylesheet_directory_uri() . '/style-login.js' );
+        $message  = __('Hi there,') . "\r\n\r\n";
+        $message .= sprintf(__("Welcome to Alpha Match here's how to log in:"), get_option('blogname')) . "\r\n\r\n";
+        $message .= wp_login_url() . "\r\n";
+        $message .= sprintf(__('Username: %s'), $user_login) . "\r\n";
+        $message .= sprintf(__('Password: %s'), $plaintext_pass) . "\r\n\r\n";
+        $message .= sprintf(__('If you have any problems, please contact us at %s.'), get_option('admin_email')) . "\r\n\r\n";
+
+        wp_mail($user_email, sprintf(__('[%s] Your username and password'), get_option('blogname')), $message);
+
+    }
 }
-add_action( 'login_enqueue_scripts', 'my_login_stylesheet' );
-
-
-
 
 
