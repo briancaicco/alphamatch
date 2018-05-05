19c9c3a7de28970b2db5e0a97bf736d6abd9018f
Brian Caicco <2147327+briancaicco@users.noreply.github.com>
2018-05-05 12:12:39 -0500

    login failure flash message

--- wp-content/themes/alphamatch-child/header.php
+++ wp-content/themes/alphamatch-child/header.php
@@ -33,7 +33,6 @@
 
         if( get_request_parameter( 'action', '' ) == 'failed' ){ 
             echo "<div class='alert alert-warning' style='margin-bottom: 0px;' >Your username or password are incorrect. Please try agian.</div>";
-            echo "<script>$('.alert').remove().fadeOut(500);</script>";
         }
         
 };?>
